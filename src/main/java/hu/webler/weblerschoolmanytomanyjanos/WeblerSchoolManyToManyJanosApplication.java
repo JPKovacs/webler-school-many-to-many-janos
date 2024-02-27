2f48675794d675869a87590425861ae9e1dbbd0b
Janos Kovacs <janos.kovacs@hotmail.hu>
2024-02-27 05:15:23 +0100

    Initial commit

---
+++ src/main/java/hu/webler/weblerschoolmanytomanyjanos/WeblerSchoolManyToManyJanosApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerschoolmanytomanyjanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerSchoolManyToManyJanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerSchoolManyToManyJanosApplication.class, args);
+	}
+
+}
